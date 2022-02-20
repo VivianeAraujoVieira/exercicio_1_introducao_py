b22c3016041107977700d717d0fea0b772e0238e
VivianeAraujo <vyviaraujo@hotmail.com.br>
2022-02-20 14:19:01 -0300

    Create exercicio_1_introducao.py

---
+++ exercicio_1_introducao.py
@@ -0,0 +1,10 @@
+# Desafio 01: o que esse pequeno trecho de código faz?
+
+
+distancia = float(input("Digite a distancia em km: "))
+
+Velocidade_média = float(input("Digite a velocidade média em km/h:"))
+
+tempo_total_horas = distancia/ Velocidade_média
+
+print(f"\nO tempo estimado é de {tempo_total_horas:.2f} hora\n")
